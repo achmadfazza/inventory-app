e78210f6dbaba2cd28ef2f503c7fe926812db18c
faza <arkadyfaza@gmail.com>
2022-11-11 10:45:59 +0700

    fix components

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,10 @@
 <div class="mb-3">
     <label class="form-label">{{ $title }}</label>
-    <input type="{{ $type }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ $value }}">
+    <input type="{{ $type }}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}"
+        placeholder="{{ $placeholder }}" value="{{ $value }}">
     @error($name)
-    <div class="invalid-feedback">
-        {{ $message }}
-    </div>
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
     @enderror
 </div>
