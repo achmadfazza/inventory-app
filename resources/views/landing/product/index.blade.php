2381fe4e07bcb1bc5471672dedc9d137aea0b2db
faza <arkadyfaza@gmail.com>
2022-12-12 14:01:56 +0700

    update new

--- resources/views/landing/product/index.blade.php
+++ resources/views/landing/product/index.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.landing.master', ['title' => 'E-Procurement'])
 
 @section('content')
-    @include('layouts.landing.partials.hero')
-    <x-landing.container>
-        <x-landing.grid class="lg:grid-cols-12 gap-6">
-            <div class="col-span-12">
-                <x-landing.header title="Daftar Produk" subtitle="Kumpulan produk"
-                    url="{{ route('product.index') }}" />
-                <x-landing.grid class="md:grid-cols-4 gap-6 items-start">
-                    @foreach ($products as $product)
-                        <x-landing.product-item :product=$product />
-                    @endforeach
-                </x-landing.grid>
-            </div>
-        </x-landing.grid>
-    </x-landing.container>
+@include('layouts.landing.partials.hero')
+<x-landing.container>
+    <x-landing.grid class="lg:grid-cols-12 gap-6">
+        <div class="col-span-12">
+            <x-landing.header title="Daftar Produk" subtitle="Kumpulan produk" url="{{ route('product.index') }}" />
+            <x-landing.grid class="md:grid-cols-4 gap-6">
+                @foreach ($products as $product)
+                <x-landing.product-item :product=$product />
+                @endforeach
+            </x-landing.grid>
+        </div>
+    </x-landing.grid>
+</x-landing.container>
 @endsection
