f888f93d3825ad827c9d75463e774b94a54fc1aa
faza <arkadyfaza@gmail.com>
2023-01-20 16:17:40 +0700

    update date

--- resources/views/layouts/backoffice/partials/navbar.blade.php
+++ resources/views/layouts/backoffice/partials/navbar.blade.php
@@ -15,8 +15,22 @@
                 <div class="dropdown-menu dropdown-menu-right">
                     <a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24" height="24"
-                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24"
+                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z"></path>
+                            <path d="M7 6a7.75 7.75 0 1 0 10 0"></path>
+                            <line x1="12" y1="4" x2="12" y2="12"></line>
+                        </svg>
+                        Logout
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24"
+                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                             stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z"></path>
                             <path d="M7 6a7.75 7.75 0 1 0 10 0"></path>
